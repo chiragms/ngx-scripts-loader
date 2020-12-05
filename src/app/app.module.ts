6252f105e8d12007c2e2f10650cf30f702befc82
Chirag <hi.chiragms@gmail.com>
2020-12-05 17:15:28 +0530

    structure update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,7 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { NgxScriptsLoaderModule } from 'projects/ngx-scripts-loader/src/lib/ngx-scripts-loader.module';
+import { NgxScriptsLoaderModule } from 'projects/@chiragms/ngx-scripts-loader/src/lib/ngx-scripts-loader.module';
 import { HighlightPlusModule } from 'ngx-highlightjs/plus';
 import { HIGHLIGHT_OPTIONS } from 'ngx-highlightjs';
 
