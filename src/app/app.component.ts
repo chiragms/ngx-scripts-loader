6e550b6a1674b4c42c9c6fe3b9aba996357cd76b
Chirag <hi.chiragms@gmail.com>
2020-12-05 14:30:38 +0530

    documentation enhancements

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { NgxScriptsLoaderService } from 'projects/ngx-scripts-loader/src/lib/ngx-scripts-loader.service';
 
+declare var $: any;
+
 @Component({
   selector: 'ngx-cm-root',
   templateUrl: './app.component.html',
@@ -8,10 +10,49 @@ import { NgxScriptsLoaderService } from 'projects/ngx-scripts-loader/src/lib/ngx
 })
 export class AppComponent {
   title = 'ngxScriptsLoaderDemo';
+  jQueryLoaded = false;
+  bsLoaded = false;
+
+  angularModuleCode = `import { NgxScriptsLoaderModule } from 'projects/ngx-scripts-loader/src/lib/ngx-scripts-loader.module';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    NgxScriptsLoaderModule, <--- import module
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
+  `;
+
+  loadCode = `
+declare var $: any;
+...
+@Component()
+...
+constructor(
+  private scriptLoader: NgxScriptsLoaderService <--- inject the service
+) { }
+
+loadJquery() {
+  // load jQuery and then execute some code
+  this.scriptLoader.load('https://code.jquery.com/jquery-3.5.1.min.js').subscribe(result => {
+    // hide all p tags using jQuery
+    $('p').hide();
+  })
+}
+  `
 
   constructor(private scriptLoader: NgxScriptsLoaderService) { }
 
   loadJquery() {
-    this.scriptLoader.load('https://code.jquery.com/jquery-3.5.1.min.js').subscribe(result => alert('Loaded'))
+    this.scriptLoader.load('https://code.jquery.com/jquery-3.5.1.min.js').subscribe(result => {
+      this.jQueryLoaded = true;
+    })
   }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,8 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NgxScriptsLoaderModule } from 'projects/ngx-scripts-loader/src/lib/ngx-scripts-loader.module';
+import { HighlightPlusModule } from 'ngx-highlightjs/plus';
+import { HIGHLIGHT_OPTIONS } from 'ngx-highlightjs';
 
 @NgModule({
   declarations: [
@@ -12,9 +14,17 @@ import { NgxScriptsLoaderModule } from 'projects/ngx-scripts-loader/src/lib/ngx-
   imports: [
     BrowserModule,
     AppRoutingModule,
-    NgxScriptsLoaderModule
+    NgxScriptsLoaderModule,
+    HighlightPlusModule
   ],
-  providers: [],
+  providers: [
+    {
+      provide: HIGHLIGHT_OPTIONS,
+      useValue: {
+        fullLibraryLoader: () => import('highlight.js')
+      }
+    }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
